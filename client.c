6612a9b8902fe0abadbe3426ba15331daac2e49e
ymori <yuu.mori06@gmail.com>
2021-07-06 18:26:12 +0200

    made: client

--- client.c
+++ client.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <signal.h>
+#include "libft/libft.h"
 
-int main(void)
+int transmitter(pid_t pid, const char *massage)
 {
-    printf("aaaa\n");
+    int i;
+    
+    i = 0;
+    while (*massage)
+    {
+        i = 0;
+        while (i < 8)
+        {
+            if (*massage & (1 << i))
+                kill(pid, SIGUSR1);
+            else
+                kill(pid, SIGUSR2);
+            usleep(100);
+            i++;
+        }
+        massage++;
+    }
+    return (0);
+}
+
+int main(int argc, char **argv)
+{
+    pid_t   pid;
+
+    if (argc != 3)
+        return (-1);
+    pid = ft_atoi(argv[1]);
+
+    transmitter(pid, argv[2]);
     return (0);
 }
